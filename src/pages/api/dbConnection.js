ca59f6b573c6873a183d138d93ca10bb4330b4fa
Narione <ynhp3214@gmail.com>
2024-05-27 19:42:22 +0900

    240527-3 dbConnection

--- src/pages/api/dbConnection.js
+++ src/pages/api/dbConnection.js
@@ -1,11 +1,30 @@
 import executeQuery from "../../../database";
 
 export default async function handler(req, res) {
-  const { id } = req.query;
-  const query = `select id,name from test_table where id = ?`;
-  console.log(query);
+  const { id, name, url } = req.body;
+  console.log("==> ", req.body);
 
-  const result = await executeQuery(query, [id]);
-  console.log(result);
-  res.status(200).json(result);
+  try {
+    switch (url) {
+      case "addData":
+        const query = `INSERT INTO test_table (id, name) VALUES(?, ?)`;
+        console.log(query);
+
+        const result = await executeQuery(query, [id, name]);
+        console.log(result);
+        if (result.affectedRows > 0) {
+          res.status(200).json("성공");
+        } else {
+          res.status(200).json("실패");
+        }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  //   const query = `select id,name from test_table where id = ?`;
+  //   console.log(query);
+  //   const result = await executeQuery(query, [id]);
+  //   console.log(result);
+  //   res.status(200).json(result);
 }
